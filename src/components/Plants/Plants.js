7d2b99d9d06b24d56b575f3d2701b9adf0275f18
Nathan Blair <nathanburrblair@gmail.com>
2019-01-15 17:10:56 -0700

    media query and grid work

--- src/components/Plants/Plants.js
+++ src/components/Plants/Plants.js
@@ -35,37 +35,57 @@ class Plants extends Component {
         {/* <div className="all_plant_list">{displayAllPlants}</div> */}
         <div className="plant_intro">
           <h2>Plants You'll Love</h2>
-          <p>This is an intro paragraph that says what amazing plants we have at our store. It encourages you to browse and buy plants.</p>
-        </div>
-        <div className="orchid">
-          <Link to="/plants/orchid">
-            <h2>Orchids</h2>
-          </Link>
           <p>
-            We only sell the most beautiful orchids in the world. Your jaw will
-            drop to the floor when you see our orchids.
+            This is an intro paragraph that says what amazing plants we have at
+            our store. It encourages you to browse and buy plants.
           </p>
-          <Link to='/plants/orchid'><button>Shop Orchids</button></Link>
         </div>
-        <div className="succulent">
-          <Link to="/plants/succulent">
-            <h2>Succulents</h2>
-          </Link>
-          <p>
-            We only sell the most beautiful succulents in the world. Your jaw will
-            drop to the floor when you see our succulents.
-          </p>
-          <Link to='/plants/succulent'><button>Shop Succulents</button></Link>
-        </div>
-        <div className="bonsai">
-          <Link to="/plants/bonsai">
-            <h2>Bonsai</h2>
-          </Link>
-          <p>
-            We only sell the most beautiful bonsai trees in the world. Your jaw will
-            drop to the floor when you see our bonsai trees.
-          </p>
-          <Link to='/plants/bonsai'><button>Shop Bonsai</button></Link>
+        <div className="sub_categories">
+          <div className="orchid">
+            <div className="orchid_overlay" />
+            <div className="orchid_text">
+              <Link to="/plants/orchid">
+                <h2>Orchids</h2>
+              </Link>
+              <p>
+                We only sell the most beautiful orchids in the world. Your jaw
+                will drop to the floor when you see our orchids.
+              </p>
+              <Link to="/plants/orchid">
+                <button>Shop Orchids</button>
+              </Link>
+            </div>
+          </div>
+          <div className="succulent">
+            <div className="succulent_overlay" />
+            <div className="succulent_text">
+              <Link to="/plants/succulent">
+                <h2>Succulents</h2>
+              </Link>
+              <p>
+                We only sell the most beautiful succulents in the world. Your
+                jaw will drop to the floor when you see our succulents.
+              </p>
+              <Link to="/plants/succulent">
+                <button>Shop Succulents</button>
+              </Link>
+            </div>
+          </div>
+          <div className="bonsai">
+            <div className="bonsai_overlay" />
+            <div className="bonsai_text">
+              <Link to="/plants/bonsai">
+                <h2>Bonsai</h2>
+              </Link>
+              <p>
+                We only sell the most beautiful bonsai trees in the world. Your
+                jaw will drop to the floor when you see our bonsai trees.
+              </p>
+              <Link to="/plants/bonsai">
+                <button>Shop Bonsai</button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     );
